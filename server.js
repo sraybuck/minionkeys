7aa90e9da19adb94d3a6dff874ff98522da36735
sraybuck <suzraybuck@gmail.com>
2020-05-12 09:51:49 -0400

    accessing db, logging contents

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//Load mysql module
+var mysql = require('mysql'); 
+
+//Load filesystem module
+var fs = require('fs');
+
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
 
@@ -16,3 +22,26 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+//get mySQL connection info from json file
+configPath = './config.json';
+var config = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+
+//set mysql host, creds and db
+var con = mysql.createConnection({
+  host: config.HOST,
+  user: config.user,
+  password: config.pass,
+  database: config.database
+});
+
+//establish connection
+con.connect(function(err){
+  if(err) throw err;
+  //query
+  con.query("SELECT email, expiration from users", function (err, result, fields){
+    if(err) throw err;
+    //print result
+    console.log(result);
+  })
+})
