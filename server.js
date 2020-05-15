f7735bee25ce0cc461ddefc57f8f63f59806582d
sraybuck <suzraybuck@gmail.com>
2020-05-15 09:18:27 -0400

    calling keycloak api with errors

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//request module
+const request = require('request-promise');
+
+//load axios
+const axios = require('axios');
+
 //Load mysql module
 var mysql = require('mysql'); 
 
@@ -29,10 +35,10 @@ var config = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 
 //set mysql host, creds and db
 var con = mysql.createConnection({
-  host: config.HOST,
-  user: config.user,
-  password: config.pass,
-  database: config.database
+  host: config.mysql.HOST,
+  user: config.mysql.user,
+  password: config.mysql.pass,
+  database: config.mysql.database
 });
 
 //establish connection
@@ -45,3 +51,40 @@ con.connect(function(err){
     console.log(result);
   })
 })
+
+function btoa(s) {
+  return Buffer.from(s, 'utf8').toString('base64')
+}
+ 
+function auth(username, password) {
+  return 'Basic ' + btoa(username + ':' + password)
+}
+request({ // Client credentials grant to get access_token
+  method: 'POST',
+  uri: config.keycloak.auth.accessTokenUri,
+  headers: {
+    'Accept': 'application/json, application/x-www-form-urlencoded',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': auth(config.keycloak.auth.clientId, config.keycloak.auth.clientSecret)
+  },
+  form: {
+    grant_type: 'client_credentials',
+  }
+})
+.then(JSON.parse)
+.then(data => data.access_token) // access_token, refresh_token, expires_in, refresh_expires_in
+.then(access_token => request({ // call to keycloak API using access token
+  method: 'GET',
+  uri: "https://localhost:8080/auth/realms/test/users",
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${access_token}`
+  },
+  body: {
+    "appId": appId, // valid appIds: eipplus-dev, eipplus-onboard, eipplus
+    "email": email // user email
+  },
+  json: true,
+}))
+
