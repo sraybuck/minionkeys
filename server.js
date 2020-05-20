e2b8e7a80c3edd191d6ae87af0bb97f3621235b4
sraybuck <suzraybuck@gmail.com>
2020-05-20 11:50:13 -0400

    calling keycloak api correctly

--- server.js
+++ server.js
@@ -1,33 +1,11 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
-
 //request module
 const request = require('request-promise');
-
-//load axios
-const axios = require('axios');
 
 //Load mysql module
 var mysql = require('mysql'); 
 
 //Load filesystem module
 var fs = require('fs');
-
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
 
 //get mySQL connection info from json file
 configPath = './config.json';
@@ -41,16 +19,19 @@ var con = mysql.createConnection({
   database: config.mysql.database
 });
 
+var test_emails = ["test1@gmail.com","test2@gmail.com","test3@gmail.com","test4@gmail.com","test5@gmail.com",]
+
 //establish connection
-con.connect(function(err){
-  if(err) throw err;
-  //query
-  con.query("SELECT email, expiration from users", function (err, result, fields){
+function connect(){
+  con.connect(function(err){
     if(err) throw err;
-    //print result
-    console.log(result);
+    //query
+    var data = con.query("SELECT email, expiration from users", function (err, result, fields){
+      if(err) throw err;
+      return result;
+    })
   })
-})
+}
 
 function btoa(s) {
   return Buffer.from(s, 'utf8').toString('base64')
@@ -59,32 +40,63 @@ function btoa(s) {
 function auth(username, password) {
   return 'Basic ' + btoa(username + ':' + password)
 }
-request({ // Client credentials grant to get access_token
-  method: 'POST',
-  uri: config.keycloak.auth.accessTokenUri,
-  headers: {
-    'Accept': 'application/json, application/x-www-form-urlencoded',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': auth(config.keycloak.auth.clientId, config.keycloak.auth.clientSecret)
-  },
-  form: {
-    grant_type: 'client_credentials',
-  }
-})
-.then(JSON.parse)
-.then(data => data.access_token) // access_token, refresh_token, expires_in, refresh_expires_in
-.then(access_token => request({ // call to keycloak API using access token
-  method: 'GET',
-  uri: "https://localhost:8080/auth/realms/test/users",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${access_token}`
-  },
-  body: {
-    "appId": appId, // valid appIds: eipplus-dev, eipplus-onboard, eipplus
-    "email": email // user email
-  },
-  json: true,
-}))
 
+async function main() {
+  const accessData = await request({ 
+      method: 'POST',
+      uri: config.keycloak.auth.accessTokenUri,
+      headers: {
+          'Accept': 'application/json, application/x-www-form-urlencoded',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': auth(config.keycloak.auth.clientId, config.keycloak.auth.clientSecret)
+      },
+      form: {
+          grant_type: 'client_credentials',
+      }
+  })
+  const accessParsed = JSON.parse(accessData)
+  const access_token = accessParsed.access_token
+
+    //get id from 
+    var userData = await request({
+      method: 'GET',
+      uri: "http://localhost:8080/auth/admin/realms/master/users?email=tfadhakdsl",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      },
+      json: true
+    })
+    console.log(userData)
+    userData=userData[9000];
+    console.log(userData);
+
+    /*
+    const representation = await request({
+      method: "GET",
+      uri: "http://localhost:8080/auth/admin/realms/master/users/92ba2261-4853-4170-a5ad-eef46553b921",
+      headers: {
+       'Accept': 'application/json',
+         'Content-Type': 'application/json',
+         'Authorization': `Bearer ${access_token}`
+       }
+    })
+    
+    var repParsed = JSON.parse(representation);
+    repParsed.enabled = false;
+    var repString = toString(repParsed);
+
+    const update = await request({
+      method: "PUT",
+      uri: "http://localhost:8080/auth/admin/realms/master/users/92ba2261-4853-4170-a5ad-eef46553b921",
+      headers: {
+       'Accept': 'application/json',
+         'Content-Type': 'application/json',
+         'Authorization': `Bearer ${access_token}`
+       },
+       body : repString
+    })
+  */
+}
+main()
